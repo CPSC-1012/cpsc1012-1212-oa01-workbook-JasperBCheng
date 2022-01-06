cbf5d527b3254860e9f1cf85ff805008bc9da007
Jasper Cheng <jcheng15@nait.ca>
2022-01-06 16:06:41 -0700

    first commit

---
+++ Simple App/Simple App/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.ForegroundColor = ConsoleColor.Red;
+Console.WriteLine("This is a Simple App");
+Console.ForegroundColor = ConsoleColor.White;
