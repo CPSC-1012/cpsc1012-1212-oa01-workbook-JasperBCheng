f28314a6091fe1f4da34b1ed2b8c7e92d4bf12e0
Jasper Cheng <jcheng15@nait.ca>
2022-01-10 16:22:58 -0700

    Learning Variables

---
+++ TabsApp/TabsApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace TabsApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("These are our top sellers: \n");         //\n Creates a new line
+            Console.WriteLine("\tItem 1\n \tItem 2 \n \tItem 3");    //\t is the same as pressing tab, giving you space in the same line
+
+            Console.WriteLine("These are our top Sellers: ");
+            Console.WriteLine("\tItem 1");
+            Console.WriteLine("\tItem 2");
+            Console.WriteLine("\tItem 3");
+        }
+    }
+}
---
+++ VariableApp/VariableApp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace VariableApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int intValue = 5;  //You can assign the value of the data type at the same time you declare it
+            Console.WriteLine($"The value is {intValue}");
+
+///////////////////////////////////////////////////////////////////////////
+
+            int checking = -20;   //We use the "int" datatype to use number values
+            long days = 189000;
+
+            Console.WriteLine($"\nOur account balance is {checking}");
+            Console.WriteLine($"About {days} days ago Columbus stood on this spot");
+
+///////////////////////////////////////////////////////////////////////////
+
+            double price, tax, total;  //You can declare multible variables at the same time
+            price = 29.75;
+            tax = 1.76;
+            total = 31.51;
+
+            Console.WriteLine($"\nThe price of the item is {price:C} \nThe tax is {tax:C} \nThe total is {total:C}");  //The :C converts the int value into currency format
+        }
+    }
+}
