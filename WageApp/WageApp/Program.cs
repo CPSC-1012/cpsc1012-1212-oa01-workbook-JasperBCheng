1212db93e6f933f239447fcb95cd943b15e8b6de
Jasper Cheng <jcheng15@nait.ca>
2022-01-12 16:51:38 -0700

    test 1

---
+++ WageApp/WageApp/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace WageApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Purpose: Compute the weekly wage of an employee
+            //Inputs: Regularl Hourly Rate
+            //        Regular Hours Worked
+            //        Overtime Hourly Rate
+            //        Overtime Hourse Worked
+
+            //Outputs:  regularWages
+            //          overtimeWages
+
+            //Algorithm:    Step 1: Prompt for regular hourly rate, regular hours worked, overtime hourly rate, overtime hourse worked
+            //              Step 2: Compute the following values:
+            //                          regularWages = regularHourlyRate * regular HoursWorked
+            //                          overtimeWages = overtimeHourlyRate * overtimeHourseWorked
+            //                          totalWages = regularWages + overtimeWages
+            //
+            //              Step 3: Display the wages for the week
+
+            // Declare variable for inputs and outputs
+            double regularHoursWorked;
+            double regularHourlyRate;
+            double overtimeHoursWorked;
+            double overtimeHourlyRate;
+            double regularWages;
+            double overtimeWages;
+            dobule totalWages;
+
+            // Prompt and read in the values for regular hourly rate
+            Console.Write($"");
+        }
+    }
+}
