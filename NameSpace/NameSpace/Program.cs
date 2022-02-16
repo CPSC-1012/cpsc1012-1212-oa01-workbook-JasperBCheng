45638812e4c5f07375a06a3b7391e174609e2693
Jasper Cheng <jcheng15@nait.ca>
2022-02-16 16:05:55 -0700

    qw

---
+++ NameSpace/NameSpace/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace NameSpace
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a Random object for generating random numbers
+            Random rand = new Random();
+            // Generate two random numbers between 1 and 99
+            int num1 = rand.Next(1, 100);
+            int num2 = rand.Next(1, 100);
+            // Conpute the correct additional answer
+            int correctAnswer = num1 + num2;
+            // Prompt the user for the correct answer
+            int userAnswer = PromptForIntergerValue($"What is {num1} + {num2} =");
+            if (userAnswer == correctAnswer)
+            {
+                Console.WriteLine($"Correct! {num1} + {num2} = {correctAnswer}");
+            }
+            else
+            {
+                Console.WriteLine($"Incorrect! The correct Answer is {correctAnswer}");
+            }
+        }
+
+        static int PromptForIntergerValue(string message)
+        {
+            int integerValue = 0;
+
+            //Prompt the user for an input
+            Console.Write(message);
+            while (int.TryParse(Console.ReadLine(), out integerValue) == false)
+            {
+                // Input Value is not an integer
+                Console.WriteLine($"Invalid Input! Please enter an Integer Value");
+                Console.Write(message);
+            }
+
+            return integerValue;
+        }
+    }
+}
