3e378b4a51c7847b7d9d66f8fabe4c58106564e8
Jasper Cheng <jcheng15@nait.ca>
2022-03-08 02:16:23 -0700

    finalized

---
+++ ArrayDemoWithMethods/ArrayDemoWithMethods/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace ArrayDemoWithMethods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Prompt the user for the number of quick picks
+
+            // For each quick pick Generate 7 random numbers between 1 and 50 by calling the GenerateNumbers methods and write the generated numbers to a text file
+            int[] lottoNumberArray = GenerateNumbers(1, 50, 7);
+            // Display all the numbers generated
+            DisplayNumbers(lottoNumberArray);
+        }
+
+        static int[] GenerateNumbers(int minValue, int maxValue, int numberCount)
+        {
+            // Declare and create a new int array of size numberCount
+            int[] numberArray = new int[numberCount];
+            // Create and create a new Random object that we can use to generate random numbers
+            Random rand = new Random();
+            // Assign a random number between minValue and maxValue for each element in the array
+            for (int index = 0; index < numberCount; index++)
+            {
+                // Generate a random number between minValue and maxValue, add the random number to the array only if it is not a duplicate
+                // Hint: You can call the isDuplicateNumber method
+                int randomNumber = rand.Next(minValue, maxValue + 1);
+                if (IsDuplicateNumber(randomNumber, numberArray))
+                {
+                    index--;
+                }
+                else
+                {
+                    numberArray[index] = randomNumber;  
+                }
+            }
+            // Sort the numberArray ascending from smallest to largest
+            // Hint: The Array class has a static class-level method for sorting 
+            Array.Sort(numberArray);
+
+            return numberArray;
+        }
+
+        static bool IsDuplicateNumber(int number, int[] numberArray)
+        {
+            bool isDuplicate = false;
+            // Set isDuplicate to true if number is in numberArray
+            for (int index = 0; index < numberArray.Length; index++)
+            {
+                if(numberArray[index] == number)
+                {
+                    isDuplicate = true;
+                    index = numberArray.Length; // Graceful technique exit loop
+                }
+            }
+
+            return isDuplicate;
+        }
+
+        static void DisplayNumbers(int[] numberArray)
+        {
+            // Display each element in numberArray where each element is separated by one empty space
+            /*Console.Write($"The winning lotto numbers are: ");
+            for (int index = 0;index < numberArray.Length; index++)
+            {
+                Console.Write($"{numberArray[index]}");
+            }*/
+            foreach (int number in numberArray)
+            {
+                Console.Write($"{number} ");
+            }
+            // Both for statements does the same thing
+            Console.WriteLine();
+        }
+
+    }
+}
---
+++ CSPC1012-Lab4-JasperCheng/CSPC1012-Lab4-JasperCheng/Program.cs
@@ -0,0 +1,158 @@
+﻿/*
+ * Write a program that has at least three methods, you may use more if you need, that prompts the user to enter exactly 3 marks then print the average of the three marks and the letter grade for the average.  The first method reads in and verifies marks from a student, call the method PromptForMark.  The marks must be between 0 to 100 and validated for invalid inputs.  The second method calculates and returns the students average, call the method CalculateAverage.  The third method determines the student letter grade, call it LetterGrade.  Use the table below to determine the letter grade.
+ * 
+ * Input: firstMark, secondMark, thirdMark, 
+ * 
+ * Output: overallAverage, gradeLetter
+ * 
+ * Written By: Jasper B. Cheng
+ * 
+ * Written For: Sam WU
+ * 
+ * Last Modified: 03/08/2022
+ * 
+ * Section (1212) (OA01)
+ * 
+ */
+
+using System;
+
+namespace CSPC1012_Lab4_JasperCheng
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int
+                firstMark,
+                secondMark,
+                thirdMark;
+
+            double
+                overallAverage;
+
+            string
+                gradeLetter;
+
+            bool
+                validateRetry = false;
+
+            while (validateRetry == false)
+            {
+                Console.WriteLine($"****************Student Grade Calculator****************");
+                Console.Write($"Input 1st Mark: ");
+                firstMark = PromptForMark();
+                Console.Write($"\nInput 2nd Mark: ");
+                secondMark = PromptForMark();
+                Console.Write($"\nInput 3rd Mark: ");
+                thirdMark = PromptForMark();
+                overallAverage = CalculateAverage(firstMark, secondMark, thirdMark);
+                gradeLetter = LetterGrade(overallAverage);
+                Console.WriteLine($"\nThe average is {overallAverage:F2}% and a student code of {gradeLetter}");
+                Console.WriteLine($"\nDo you want to try again? (Y)|(N)");
+                validateRetry = ValidateUserRetry();
+            }
+        }
+
+        static int PromptForMark()
+        {
+            int selectedValue = 0;
+            bool validateInput = false;
+            while (validateInput == false)
+            {
+                if (int.TryParse(Console.ReadLine(), out selectedValue))
+                {
+                    if (selectedValue >= 0 && selectedValue <= 100)
+                    {
+                        validateInput = true;
+                    }
+                    else if (selectedValue > 100)
+                    {
+                        Console.WriteLine($"\nInvalid Input!");
+                        Console.WriteLine($"Please enter a number between 1 to 100");
+                        Console.Write($"> ");
+                    }
+                    else if (selectedValue < 0)
+                    {
+                        Console.WriteLine($"\nInvalid Input!");
+                        Console.WriteLine($"Please enter a positive number");
+                        Console.Write($"> ");
+                    }  
+                }
+                else
+                {
+                    Console.WriteLine($"\nInput string was not in a correct format");
+                    Console.WriteLine($"Please input a positive integer");
+                    Console.Write($"> ");
+                }
+            }
+            return selectedValue;
+        }
+
+        static double CalculateAverage(int firstMark, int secondMark, int thirdMark)
+        {
+            const int
+                MaxMark = 300,
+                AverageMultiplier = 100;
+            double totalMark = firstMark + secondMark + thirdMark;
+            double totalAverage = (totalMark / MaxMark) * AverageMultiplier;
+            return totalAverage;
+        }
+
+        static string LetterGrade(double overallAverage)
+        {
+            string gradeLetter = "default";
+            if (overallAverage >= 80)
+            {
+                gradeLetter = "A";
+            }
+            else if(overallAverage < 80 && overallAverage >= 65)
+            {
+                gradeLetter= "B";
+            }
+            else if(overallAverage < 65 && overallAverage >= 50)
+            {
+                gradeLetter = "C";
+            }
+            else if(overallAverage < 50 && overallAverage >= 40)
+            {
+                gradeLetter = "D";
+            }
+            else if(overallAverage < 40)
+            {
+                gradeLetter = "F";
+            }
+            return gradeLetter;
+        }
+
+        static bool ValidateUserRetry()
+        {
+            bool
+                validateInput = false,
+                validateRetry = false;
+            while (validateInput == false)
+            {
+                Console.Write($"> ");
+                string userRetry = Console.ReadLine();
+                if (userRetry == "y" || userRetry == "Y")
+                {
+                    validateRetry = false;
+                    validateInput = true;
+                }
+                else if (userRetry == "n" || userRetry == "N")
+                {
+                    validateRetry = true;
+                    validateInput = true;
+                }
+                else
+                {
+                    Console.WriteLine($"\nInvalid Input!");
+                    Console.WriteLine($"Please input either (Y) or (N)");
+                }
+            }
+            Console.WriteLine($"");
+            return validateRetry;
+        }
+    }
+
+}
